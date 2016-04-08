b92d5ece77891f20ca8ec5ae621b8c378ae7aee4
m-dl <fullcontactsoldier@gmail.com>
2016-04-08 16:26:40 +0200

    MAJ

--- src/main/java/com/google/api/services/drive/cmdline/DriveTools.java
+++ src/main/java/com/google/api/services/drive/cmdline/DriveTools.java
@@ -13,7 +13,6 @@ import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.Preconditions;
 import com.google.api.client.util.store.DataStoreFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
@@ -21,6 +20,7 @@ import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -35,12 +35,16 @@ public class DriveTools {
    * blank, the application will log a warning. Suggested format is "MyCompany-ProductName/1.0".
    */
   private static final String APPLICATION_NAME = "Backend_Fruitière_Numérique/1.0";
-  private static final String VILLAGE_MEDIAS = "medias_village";
-  private static final String CHATEAU_MEDIAS = "medias_chateau";
+  private static final String VILLAGE_MEDIAS = "VisiteTablette";
+  private static final String CHATEAU_MEDIAS = "VisiteChateau";
+  private static final String APP_ZIP = "application/zip";
   private static final String ZIP_EXT = ".zip";
-  private static final String UPLOAD_FILE_PATH = "C:/Users/Maxime.PC/Desktop/medias.zip";
-  private static final String DIR_FOR_DOWNLOADS = "C:/Users/Maxime.PC/Desktop/";
-  private static final java.io.File UPLOAD_FILE = new java.io.File(UPLOAD_FILE_PATH);
+  private static final String MEDIAS = "medias";
+  private static final String UPLOAD_FILE_PATH_VILLAGE = MEDIAS + "/VisiteTablette.zip";
+  private static final String UPLOAD_FILE_PATH_CHATEAU = MEDIAS + "/VisiteChateau.zip";
+  private static final String DIR_FOR_DOWNLOADS = MEDIAS;
+  private static final java.io.File UPLOAD_FILE_VILLAGE = new java.io.File(UPLOAD_FILE_PATH_VILLAGE);
+  private static final java.io.File UPLOAD_FILE_CHATEAU = new java.io.File(UPLOAD_FILE_PATH_CHATEAU);
   /** Directory to store user credentials. */
   private static final java.io.File DATA_STORE_DIR = new java.io.File(
       System.getProperty("user.home"), ".store/drive_sample");
@@ -65,11 +69,10 @@ public class DriveTools {
     // load client secrets
     GoogleClientSecrets clientSecrets =
         GoogleClientSecrets.load(JSON_FACTORY,
-            new InputStreamReader(DriveTools.class.getResourceAsStream("/client_secrets.json")));
-    if (clientSecrets.getDetails().getClientId().startsWith("Enter")
-        || clientSecrets.getDetails().getClientSecret().startsWith("Enter ")) {
+            new InputStreamReader(new FileInputStream("res/client_secrets.json")));
+    if (clientSecrets.getDetails().getClientId().equals("") || clientSecrets.getDetails().getClientSecret().equals("")) {
       System.out.println("Enter Client ID and Secret from https://code.google.com/apis/console/?api=drive "
-              + "into drive-cmdline-sample/src/main/resources/client_secrets.json");
+              + "into res/client_secrets.json");
       System.exit(1);
     }
     // set up authorization code flow
@@ -80,11 +83,7 @@ public class DriveTools {
     return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
   }
 
-  public static void launch() {
-    Preconditions.checkArgument(
-        !UPLOAD_FILE_PATH.startsWith("Enter ") && !DIR_FOR_DOWNLOADS.startsWith("Enter "),
-        "Please enter the upload file path and download directory in %s", DriveTools.class);
-
+  public static boolean auth() {
     try {
       httpTransport = GoogleNetHttpTransport.newTrustedTransport();
       dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
@@ -92,42 +91,84 @@ public class DriveTools {
       Credential credential = authorize();
       // set up the global Drive instance
       drive = new Drive.Builder(httpTransport, JSON_FACTORY, credential).setApplicationName(APPLICATION_NAME).build();
-
-      // run commands
-
-      View.header1("Starting Resumable Media Upload");
-      File uploadedFile = uploadFile(false);
-
-      View.header1("Updating Uploaded File Name");
-      File updatedFile = updateFileWithTestSuffix(uploadedFile.getId());
-
-      deleteFile(CHATEAU_MEDIAS);
-
-      View.header1("Starting Resumable Media Download");
-      downloadFile(false, uploadedFile);
-/*
-      View.header1("Starting Simple Media Upload");
-      uploadedFile = uploadFile(true);
-
-      View.header1("Starting Simple Media Download");
-      downloadFile(true, uploadedFile);
-*/
-      View.header1("Success!");
-      return;
+      return true;
     } catch (IOException e) {
       System.err.println(e.getMessage());
     } catch (Throwable t) {
       t.printStackTrace();
     }
-    System.exit(1);
+    return false;
+  }
+  
+  public static void upload(String file, String fileName) {
+    try {
+      if(auth()) {
+	      // run commands
+	
+    	  java.io.File f = new java.io.File(file);
+    	  View.header1("Deleting old files from Drive");
+    	  deleteFile(fileName);
+    	  
+	      View.header1("Starting Resumable Media Upload");
+	      uploadFile(false, f);
+
+	      View.header1("Success!");
+      }
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    } catch (Throwable t) {
+      t.printStackTrace();
+    }
+  }
+  
+  public static void download(String fileName) {
+    try {
+      if(auth()) {
+	      // run commands
+    	  
+	      View.header1("Starting Resumable Media Download");
+	      File file = searchFileByFile(fileName);
+	      if(file != null)
+	    	  downloadFile(false, file);
+	      else
+	    	  View.header1("Error, file not found");
+
+	      View.header1("Success!");
+      }
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    } catch (Throwable t) {
+      t.printStackTrace();
+    }
   }
 
+  public static void rename(String pattern) {
+    try {
+      if(auth()) {
+	      // run commands
+	
+	      View.header1("Updating Uploaded File Name");
+	      ArrayList<String> file = searchFile(pattern);
+	      if(!file.isEmpty())
+	    	  updateFileWithTestSuffix(file.get(0));
+	      else
+	    	  View.header1("Error, file not found");
+	      
+	      View.header1("Success!");
+      }
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    } catch (Throwable t) {
+      t.printStackTrace();
+    }
+  }
+  
   /** Uploads a file using either resumable or direct media upload. */
-  private static File uploadFile(boolean useDirectUpload) throws IOException {
+  private static File uploadFile(boolean useDirectUpload, java.io.File uploadFile) throws IOException {
     File fileMetadata = new File();
-    fileMetadata.setTitle(UPLOAD_FILE.getName());
+    fileMetadata.setTitle(uploadFile.getName());
 
-    FileContent mediaContent = new FileContent("application/zip", UPLOAD_FILE);
+    FileContent mediaContent = new FileContent(APP_ZIP, uploadFile);
 
     Drive.Files.Insert insert = drive.files().insert(fileMetadata, mediaContent);
     MediaHttpUploader uploader = insert.getMediaHttpUploader();
@@ -148,7 +189,7 @@ public class DriveTools {
 	    }
 	}
   }
-  
+
   /**
    * Search and delete file list matching the pattern.
    *
@@ -169,19 +210,19 @@ public class DriveTools {
   }
 
   /** Downloads a file using either resumable or direct media download. */
-  private static void downloadFile(boolean useDirectDownload, File uploadedFile) throws IOException {
+  private static void downloadFile(boolean useDirectDownload, File file) throws IOException {
     // create parent directory (if necessary)
     java.io.File parentDir = new java.io.File(DIR_FOR_DOWNLOADS);
     if (!parentDir.exists() && !parentDir.mkdirs()) {
       throw new IOException("Unable to create parent directory");
     }
-    OutputStream out = new FileOutputStream(new java.io.File(parentDir, uploadedFile.getTitle()));
+    OutputStream out = new FileOutputStream(new java.io.File(parentDir, file.getTitle()));
 
     MediaHttpDownloader downloader =
         new MediaHttpDownloader(httpTransport, drive.getRequestFactory().getInitializer());
     downloader.setDirectDownloadEnabled(useDirectDownload);
     downloader.setProgressListener(new FileDownloadProgressListener());
-    downloader.download(new GenericUrl(uploadedFile.getDownloadUrl()), out);
+    downloader.download(new GenericUrl(file.getDownloadUrl()), out);
   }
   
   /** Search files by pattern : title, name, description ... */
@@ -203,5 +244,23 @@ public class DriveTools {
 		}
 	  } while (pageToken != null);
 	  return match;
+  }
+  
+  /** Search files by pattern : title, name, description ... */
+  private static File searchFileByFile(String pattern) {
+	  String pageToken = null;
+	  do {
+	      FileList result;
+		try {
+			result = drive.files().list().setQ("fullText contains '" + pattern + "'").execute();
+			if(!result.getItems().isEmpty())
+				return result.getItems().get(0);
+	      pageToken = result.getNextPageToken();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	  } while (pageToken != null);
+	  return null;
   }
 }
